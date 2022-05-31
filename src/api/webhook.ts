793c3930428b39171073ac1bfb4025b89f37bff0
JC Santiago <santiago.benavides-ext@dominion-global.com>
2022-05-30 21:52:10 -0500

    feat: added notify close alert in webhook

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -229,9 +229,51 @@ async function notifyNoteCreated(request: WebhookRequest<NoteWebhook>, headers: 
     await mattermostClient.incomingWebhook(payload);
 }
 
+async function notifyCloseAlert(request: WebhookRequest<AlertWebhook>, headers: { [key: string]: any }) {
+    const mattermostWebhookUrl: string = headers[AppMattermostConfig.WEBHOOK];
+    const mattermostUrl: string = 'http://127.0.0.1:8066';
+    const alert: AlertWebhook = request.alert;
+
+    const optionsOpsgenie: OpsGenieOptions = {
+        api_key: config.OPSGENIE.API_KEY
+    };
+    const opsGenieClient = new OpsGenieClient(optionsOpsgenie);
+
+    const account: ResponseResultWithData<Account> = await opsGenieClient.getAccount();
+
+    const url: string = `${AppsOpsGenie}${Routes.OpsGenieWeb.AlertDetailPathPrefix}`;
+    const alertDetailUrl: string = replace(
+        replace(
+            url,
+            Routes.PathsVariable.Account,
+            account.data.name
+        ),
+        Routes.PathsVariable.Identifier,
+        alert.alertId
+    );
+    const payload = {
+        text: '',
+        username: 'opsgenie',
+        icon_url: `${config.APP.HOST}/static/opsgenie_picture.png`,
+        attachments: [
+            {
+                text: `${alert.username} closed alert #${hyperlink(alert.tinyId, alertDetailUrl)} "${alert.message}"`,
+            }
+        ]
+    };
+
+    const mattermostOptions: MattermostOptions = {
+        mattermostUrl: mattermostWebhookUrl,
+        accessToken: null
+    };
+    const mattermostClient: MattermostClient = new MattermostClient(mattermostOptions);
+    await mattermostClient.incomingWebhook(payload);
+}
+
 const WEBHOOKS_ACTIONS: { [key: string]: Function } = {
     Create: notifyAlertCreated,
-    AddNote: notifyNoteCreated
+    AddNote: notifyNoteCreated,
+    Close: notifyCloseAlert
 };
 
 export const incomingWebhook = async (request: Request, response: Response) => {
