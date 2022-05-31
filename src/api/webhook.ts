46a2e700a9b71e0675a2332efd90ce064f77275c
JC Santiago <santiago.benavides-ext@dominion-global.com>
2022-05-30 23:55:16 -0500

    feat: added notify ack alert in webhook

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -216,7 +216,7 @@ async function notifyNoteCreated(request: WebhookRequest<NoteWebhook>, headers: 
         icon_url: `${config.APP.HOST}/static/opsgenie_picture.png`,
         attachments: [
             {
-                text: `${alert.username} added note "${alert.note}" to alert #${hyperlink(alert.tinyId, alertDetailUrl)} "${alert.message}"`,
+                text: `${alert.username} added note "${alert.note}" to alert ${hyperlink(`#${alert.tinyId}`, alertDetailUrl)} "${alert.message}"`,
             }
         ]
     };
@@ -257,7 +257,48 @@ async function notifyCloseAlert(request: WebhookRequest<AlertWebhook>, headers: 
         icon_url: `${config.APP.HOST}/static/opsgenie_picture.png`,
         attachments: [
             {
-                text: `${alert.username} closed alert #${hyperlink(alert.tinyId, alertDetailUrl)} "${alert.message}"`,
+                text: `${alert.username} closed alert ${hyperlink(`#${alert.tinyId}`, alertDetailUrl)} "${alert.message}"`,
+            }
+        ]
+    };
+
+    const mattermostOptions: MattermostOptions = {
+        mattermostUrl: mattermostWebhookUrl,
+        accessToken: null
+    };
+    const mattermostClient: MattermostClient = new MattermostClient(mattermostOptions);
+    await mattermostClient.incomingWebhook(payload);
+}
+
+async function notifyAckAlert(request: WebhookRequest<AlertWebhook>, headers: { [key: string]: any }) {
+    const mattermostWebhookUrl: string = headers[AppMattermostConfig.WEBHOOK];
+    const mattermostUrl: string = 'http://127.0.0.1:8066';
+    const alert: AlertWebhook = request.alert;
+
+    const optionsOpsgenie: OpsGenieOptions = {
+        api_key: config.OPSGENIE.API_KEY
+    };
+    const opsGenieClient = new OpsGenieClient(optionsOpsgenie);
+
+    const account: ResponseResultWithData<Account> = await opsGenieClient.getAccount();
+
+    const url: string = `${AppsOpsGenie}${Routes.OpsGenieWeb.AlertDetailPathPrefix}`;
+    const alertDetailUrl: string = replace(
+        replace(
+            url,
+            Routes.PathsVariable.Account,
+            account.data.name
+        ),
+        Routes.PathsVariable.Identifier,
+        alert.alertId
+    );
+    const payload = {
+        text: '',
+        username: 'opsgenie',
+        icon_url: `${config.APP.HOST}/static/opsgenie_picture.png`,
+        attachments: [
+            {
+                text: `${alert.username} acknowledged alert ${hyperlink(`#${alert.tinyId}`, alertDetailUrl)} "${alert.message}"`,
             }
         ]
     };
@@ -273,7 +314,8 @@ async function notifyCloseAlert(request: WebhookRequest<AlertWebhook>, headers: 
 const WEBHOOKS_ACTIONS: { [key: string]: Function } = {
     Create: notifyAlertCreated,
     AddNote: notifyNoteCreated,
-    Close: notifyCloseAlert
+    Close: notifyCloseAlert,
+    Acknowledge: notifyAckAlert
 };
 
 export const incomingWebhook = async (request: Request, response: Response) => {
